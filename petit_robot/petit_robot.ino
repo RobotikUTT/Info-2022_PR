c1f4fd5afa00ecbaf25b8fa77f83f2cb36dde118
k8pl3r-sh <64953672+k8pl3r-sh@users.noreply.github.com>
2022-05-26 16:43:32 +0200

    Create petit_robot.ino

---
+++ petit_robot/petit_robot.ino
@@ -0,0 +1,264 @@
+#include <Arduino.h>
+#include <Encoder.h>
+#include <SoftwareSerial.h>
+
+// Test de suiveur de ligne deux capteurs CNY70
+// "Petit Robot", Robotik UTT, Coupe de france 2022
+
+// Pin configuration
+#define pin_eye_R A0
+#define pin_eye_L A1
+
+// Motor pins
+
+// a pour reculer, b pour avancer
+#define pin_en_L 2 // enable
+// PIN ENABLE mais c'est du PWM
+
+
+
+#define pin_a_L 5 // a=1 et b=0  AVANCER et b=1 et a=0 pour RECULER
+#define pin_b_L 6
+#define pin_a_R 3
+#define pin_b_R 4
+#define pin_en_R 7
+
+#define tirette_pin 8
+
+SoftwareSerial nano(11, 10); // line série nano et méga
+
+void setup() {
+    Serial.begin(9600);
+
+    for (int i = 2; i <= 7; i++)
+        pinMode(i, OUTPUT);
+    pinMode(pin_eye_L, INPUT);
+    pinMode(pin_eye_R, INPUT);
+    pinMode(tirette_pin, INPUT_PULLUP);
+
+    Serial.print("[DEBUG] Attente tirette... ");
+    while (!digitalRead(tirette_pin)) {}
+    Serial.println("[DEBUG] START");
+    delay(5);
+
+    // par défaut on avance
+    digitalWrite(pin_a_L, LOW);
+    digitalWrite(pin_a_R, LOW);
+    digitalWrite(pin_b_L, HIGH); 
+    digitalWrite(pin_b_R, HIGH);
+
+    nano.begin(9600);
+    nano.listen();
+    /* DEBUG Reception Mega/Nano
+    while (true) {
+        int val = -1;
+        while (nano.available() > 0){
+            val &= nano.read();
+        }
+        Serial.print(!!(val & 1));
+        Serial.print(", ");
+        Serial.print(!!(val & 2));
+        Serial.print(", ");
+        Serial.print(!!(val & 4));
+        Serial.print(", ");
+        Serial.print(!!(val & 8));
+        Serial.println(";");
+        delay(100);
+    }*/
+
+    findLine(); // procédure robot va chercher la ligne
+}
+
+void loop() {
+    int sensor_R = analogRead(pin_eye_R);
+    int sensor_L = analogRead(pin_eye_L);
+
+    Serial.print(sensor_L);
+    Serial.print(", ");
+    Serial.print(sensor_R);
+    Serial.print(", ");
+
+    int speed_R = sensorRMotorTF(sensor_L, sensor_R);
+    int speed_L = sensorLMotorTF(sensor_L, sensor_R);
+
+    Serial.print(speed_L);
+    Serial.print(", ");
+    Serial.println(speed_R);
+
+    if (speed_R < 0) {
+        speed_R = -speed_R;
+        digitalWrite(pin_a_R, HIGH);
+        digitalWrite(pin_b_R, LOW);
+    } else {
+        digitalWrite(pin_a_R, LOW);
+        digitalWrite(pin_b_R, HIGH);
+    }
+
+    if (speed_L < 0) {
+        speed_L = -speed_L;
+        digitalWrite(pin_a_L, HIGH);
+        digitalWrite(pin_b_L, LOW);
+    } else {
+        digitalWrite(pin_a_L, LOW);
+        digitalWrite(pin_b_L, HIGH);
+    }
+
+    if (isBlocked()) {
+        analogWrite(pin_en_R, 0);
+        analogWrite(pin_en_L, 0);
+        Serial.println("Stop...");
+        delay(3000);
+        if (isBlocked()) {
+            pullOut();
+        }
+    }
+
+
+    analogWrite(pin_en_R, speed_L);
+    analogWrite(pin_en_L, speed_R);
+
+    
+}
+
+bool isBlocked() {
+    bool test = false;
+    int sensor = -1;
+    while (nano.available() > 0) {
+        sensor &= nano.read();
+        test = true;
+    }
+    if (!test)
+        sensor = 0;
+    return !!(sensor & 0b1100);
+}
+
+void pullOut() {
+    digitalWrite(pin_a_L, HIGH);
+    digitalWrite(pin_b_L, LOW);
+    digitalWrite(pin_a_R, HIGH);
+    digitalWrite(pin_b_R, LOW);
+    analogWrite(pin_en_R, 100);
+    analogWrite(pin_en_L, 100);
+
+    delay(500);
+
+    analogWrite(pin_en_R, 0);
+    analogWrite(pin_en_L, 0);
+
+    while(true);
+}
+
+void SeRetourner () { // Se retourner sur la ligne
+    int DemiTour = 0;
+    while (DemiTour < 2) { 
+        int sensor_R = analogRead(pin_eye_R);
+        int sensor_L = analogRead(pin_eye_L);
+        
+        digitalWrite(pin_a_L, LOW);
+        digitalWrite(pin_b_L, HIGH);
+        digitalWrite(pin_a_R, HIGH);
+        digitalWrite(pin_b_R, LOW);
+        analogWrite(pin_en_R, 100);
+        analogWrite(pin_en_L, 100);
+
+        if ((DemiTour == 0)&&(sensor_R < 500) ){
+            DemiTour++;
+        }
+        if ((DemiTour == 1)&&(sensor_L < 500) ){
+            //realignement
+            digitalWrite(pin_a_R, HIGH);
+            digitalWrite(pin_b_R, LOW);
+            digitalWrite(pin_a_L, LOW);
+            digitalWrite(pin_b_L, HIGH);
+            analogWrite(pin_en_R, 100);
+            analogWrite(pin_en_L, 100);
+            delay(200);
+            DemiTour++;
+        }
+    }
+}
+
+void findLine() {
+    int other = -1; // variable pour attendre quel capteur est le second à toucher la ligne
+
+    Serial.println("Sequence commencee");
+    analogWrite(pin_en_R, 100);
+    analogWrite(pin_en_L, 100);
+
+    while (true) {
+        int sensor_R = analogRead(pin_eye_R);
+        int sensor_L = analogRead(pin_eye_L);
+
+        if (other >= 0) {
+            if (analogRead(other) < 500) {
+                Serial.println("Sequence terminee");
+                digitalWrite(pin_a_R, HIGH);
+                digitalWrite(pin_b_R, LOW);
+                digitalWrite(pin_a_L, HIGH);
+                digitalWrite(pin_b_L, LOW);
+                analogWrite(pin_en_R, 120);
+                analogWrite(pin_en_L, 100);
+                delay(200);
+                break;
+            }
+        } else {
+            if (sensor_L < 500) {
+                // analogWrite(pin_en_R, 100);
+                // analogWrite(pin_en_L, 80);
+                other = pin_eye_R;
+                Serial.println("Trouve ligne gauche");
+            }
+            if (sensor_R < 500) {
+                // analogWrite(pin_en_R, 80);
+                // analogWrite(pin_en_L, 80);
+                other = pin_eye_L;
+                Serial.println("Trouve ligne droite");
+                analogWrite(pin_en_L, 50);
+            }
+                
+        }
+    }
+}
+
+int sensorRMotorTF(int sensor_L, int sensor_R) { // fonction de transfert proportionnel: valeurs capteurs -> valeurs moteurs
+    //return sensor_L * motor_speed / (1024);
+
+    if (sensor_L < 500)
+        return -60;
+    if (sensor_L > 500)
+        return 80;
+    //int bonus = (sensor_R < 850) ? 20 : 0;
+    //return (sensor_L - 800) / 2 + bonus;
+}
+
+int sensorLMotorTF(int sensor_L, int sensor_R) {
+    //return sensor_R * motor_speed / (1024);
+
+    if (sensor_R < 500)
+        return -60;
+    if (sensor_R > 500)
+        return 80;
+    //int bonus = (sensor_L < 850) ? 20 : 0; // chgmt vitesse moteur exté virage de suivi ligne
+    //return (sensor_R - 800) / 2 + bonus;
+}
+
+/*
+void forwardNmeters(float meters) { // erreur de compil
+    int init_left = left_wheel.readAndReset();
+    int init_right = right_wheel.readAndReset();
+    int ticks = meters * TICKS_PER_METER;
+
+    while (true) {
+        int right = right_wheel.read();
+        int left = left_wheel.read();
+
+        int diff = right - left;
+
+        analogWrite(PIN_EN_L, MOTOR_SPEED + diff);
+        analogWrite(PIN_EN_R, MOTOR_SPEED - diff);
+
+        if (right >= ticks && right >= ticks)
+            break;
+    }
+}
+*/
